ba4545e3cb828212c23e0c5e667842af238ef8bc
BruniDev <hyeondxx@gmail.com>
2024-05-14 18:58:37 +0900

    Initial Commit

---
+++ VCClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VCClock
+//
+//  Created by 정현 on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
